3f93d79ddca90e8b9ca7bbe5c0c2d9509fee8033
hnbreyer <53955491+hnbreyer@users.noreply.github.com>
2020-08-04 16:20:49 -0300

    Update 144_w3_lab.c

--- IPC144/Week03_TemperatureAnalyzer/144_w3_lab.c
+++ IPC144/Week03_TemperatureAnalyzer/144_w3_lab.c
@@ -3,13 +3,13 @@
 // Put your code below:
 #include <stdio.h>
 #define NUMS 3
-int main(void)                                                          //initializes the code
+int main(void)                                                                          //initializes the code
 {
-	int i, high, low;                                                    //define variables
+	int i, high, low;                                                               //define variables
 
 	printf("---=== IPC Temperature Analyzer ===---\n");
 
-	for (i = 1; i <= NUMS; i++) {                                        //for loop
+	for (i = 1; i <= NUMS; i++) {                                                    //for loop
 		
 			printf("Enter the high value for day %d: ", i);                 //printf to ask user entry
 			scanf("%d", &high);                                             // scanf_s to accept user entry
@@ -18,7 +18,7 @@ int main(void)                                                          //initia
 			scanf("%d", &low);
 			printf("\n");
 
-		while (high > 40 || low < -40 || high < low)                      //condition in nested while
+		while (high > 40 || low < -40 || high < low)                            //condition in nested while
 		{
 			printf("Incorrect values, temperatures must be in the range");
                         printf("-40 to 40, high must be greater than low.\n\n"); 
@@ -31,5 +31,34 @@ int main(void)                                                          //initia
 			printf("\n");
 		}		
 	}
-	return 0;                                                           //ends code
+	return 0;                                                                      //ends code
 }
+
+/*OUTPUT
+---=== IPC Temperature Analyzer ===---
+Enter the high value for day 1: 8
+
+Enter the low value for day 1: -2
+
+Enter the high value for day 2: 41
+
+Enter the low value for day 2: -4
+
+Incorrect values, temperatures must be in the range -40 to 40, high must be greater than low.
+
+Enter the high value for day 2: 9
+
+Enter the low value for day 2: -4
+
+Enter the high value for day 3: 5
+
+Enter the low value for day 3: 11
+
+Incorrect values, temperatures must be in the range -40 to 40, high must be greater than low.
+
+Enter the high value for day 3: 11
+
+Enter the low value for day 3: 5
+
+The average (mean) temperature was: 4.50
+*/
