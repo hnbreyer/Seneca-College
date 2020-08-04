6002a340b1ff7e5905c769cbad197a84dd36236e
hnbreyer <53955491+hnbreyer@users.noreply.github.com>
2020-08-04 16:59:15 -0300

    Update 144_w5_home.c

--- IPC144/Week05_EmployeeDataProgram/144_w5_home.c
+++ IPC144/Week05_EmployeeDataProgram/144_w5_home.c
@@ -1,22 +1,9 @@
-// Expand the option list to include option 3 & 4 after 
-// option 2. as following
-// "3. Update Employee Salary"
-// "4. Remove Employee"
-
-// Create two switch-cases for option 3 & 4 after case 2. 
-// Implement "Update Salary" and "Remove Employee" 
-// functionality as per instructions
-// inside the relevant case statements
-
 #define _CRT_SECURE_NO_WARNINGS
-// Define Number of Employees "SIZE" to be 2
-#define SIZE 4		
 
+#define SIZE 4		
 #include <stdio.h>
 
-
-// Declare Struct Employee 
-struct Employee
+struct Employee                                                                     // Declare Struct Employee 
 {
 	int ID_no;
 	int age;
@@ -27,19 +14,15 @@ struct Employee
 int main(void)
 {
 
-
-	// Declare a struct Employee array "emp" with SIZE elements 
-	// and initialize all elements to zero
-	struct Employee	emp[SIZE] = { {0} };
-	int i, valEmp = 0, idSelected, option = 0;
+	
+	struct Employee	emp[SIZE] = { {0} };                                     // Declare a struct Employee array "emp" with SIZE elements 
+	int i, valEmp = 0, idSelected, option = 0;                               // initialize all elements to zero
 	
 
 	printf("---=== EMPLOYEE DATA ===---\n\n");
 
 	do
 	{
-		//	option = 0;
-			// Print the option list
 		printf("1. Display Employee Information\n");
 		printf("2. Add Employee\n");
 		printf("3. Update Employee Salary\n");
@@ -47,57 +30,45 @@ int main(void)
 		printf("0. Exit\n\n");
 		printf("Please select from the above options: ");
 
-		// Capture input to option variable
-		scanf("%d", &option);
+		scanf("%d", &option);                                      	// Capture input to option variable
 		printf("\n");
 
 		switch (option)
 		{
 
-		case 0:	// Exit the program
+		case 0:	                                                        // Exit the program
 			printf("Exiting Employee Data Program. Good Bye!!!\n");
 			break;
 
-		case 1: // Display Employee Data
-				// @IN-LAB
+		case 1:                                                         // Display Employee Data
 
 			printf("EMP ID  EMP AGE EMP SALARY\n");
 			printf("======  ======= ==========\n");
 
-			// Use "%6d%9d%11.2lf" formatting in a   
-			// printf statement to display
-			// employee id, age and salary of 
-			// all  employees using a loop construct 
+			for (i = 0; i < SIZE; i++) { 	                        // loop will run for SIZE times 
 
-			// The loop construct will be run for SIZE times 
-			// and will only display Employee data 
-			// where the EmployeeID is > 0
-
-			for (i = 0; i < SIZE; i++) {
-
-				if (emp[i].ID_no > 0)
+				if (emp[i].ID_no > 0)                           // and will only display Employee data where the EmployeeID is > 0
 					printf("%6d%9d%11.2lf\n", emp[i].ID_no, emp[i].age, emp[i].sal);
 			}
 			printf("\n");
 			break;
 
-		case 2:	// Adding Employee
-				// @IN-LAB
+		case 2:	                                                       // Adding Employee
 
 			i = 0;
 			printf("Adding Employee\n");
 			printf("===============\n");
-			while (emp[i].ID_no != 0) //condition
+			while (emp[i].ID_no != 0) 
 				i++;
 
-			if (i < SIZE) {  //checks for empty slots in the array
+			if (i < SIZE) {                                       //checks for empty slots in the array
 				printf("Enter Employee ID: ");
 				scanf("%d", &emp[i].ID_no);
 				printf("Enter Employee Age: ");
 				scanf("%d", &emp[i].age);
 				printf("Enter Employee Salary: ");
 				scanf("%lf", &emp[i].sal);
-				valEmp++; //increment the number of employees
+				valEmp++;                                    //increment the number of employees
 
 				printf("\n");
 			}
@@ -106,7 +77,7 @@ int main(void)
 			}
 			break;
 
-		case 3:	// Update Employee Salary
+		case 3:	                                                    // Update Employee Salary
 
 			printf("Update Employee Salary\n");
 			printf("======================\n");
@@ -133,7 +104,7 @@ int main(void)
 
 			break;
 
-		case 4:	// Remove Employee
+		case 4:	                                                     // Remove Employee
 
 
 			printf("Remove Employee\n");
@@ -151,7 +122,7 @@ int main(void)
 
 			} while (i == SIZE);
 			printf("Employee %d will be removed\n", emp[i].ID_no);
-			emp[i].ID_no = 0; //removing id from array
+			emp[i].ID_no = 0;                                   //removing id from array
 			printf("\n");
 
 			break;
