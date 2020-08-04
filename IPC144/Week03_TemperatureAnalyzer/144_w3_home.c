77f65cb7fba911cfea8c9c667c95a9c96f903556
hnbreyer <53955491+hnbreyer@users.noreply.github.com>
2020-08-04 16:17:04 -0300

    Update 144_w3_home.c

--- IPC144/Week03_TemperatureAnalyzer/144_w3_home.c
+++ IPC144/Week03_TemperatureAnalyzer/144_w3_home.c
@@ -2,27 +2,28 @@
 
 // Put your code below:
 #include <stdio.h>
-#define NUMS 4 //define macro - THIS IS THE ONE CHANGE (change from 3 to 4)
-int main(void) //initializes the code
+#define NUMS 4 
+int main(void)                                                                 //initializes the code
 {
-	int i, high, low, highDay, lowDay; //define variables
-	int highestT = 0, lowestT = 0; //variables set to 0 to store values
+	int i, high, low, highDay, lowDay;                                     //define variables
+	int highestT = 0, lowestT = 0;                                         //variables set to 0 to store values
 	float mean, totHigh = 0, totLow = 0, meanHigh, meanLow;
 
 	printf("---=== IPC Temperature Analyzer ===---\n");
 
-	for (i = 0; i < NUMS; i++) {                            //for loop
+	for (i = 0; i < NUMS; i++) {                            
 
-		printf("Enter the high value for day %d: ", i+1); //printf to ask user entry; "i+1" to start from 1
-		scanf("%d", &high);                           // scanf_s to accept user entry
+		printf("Enter the high value for day %d: ", i+1);              //printf to ask user entry; "i+1" to start from 1
+		scanf("%d", &high);                                            //scanf_s to accept user entry
 		printf("\n");
 		printf("Enter the low value for day %d: ", i+1);
 		scanf("%d", &low);
 		printf("\n");
 
-		while (high > 40 || low < -40 || high < low)      //condition in nested while
+		while (high > 40 || low < -40 || high < low)                   //condition in nested while
 		{
-			printf("Incorrect values, temperatures must be in the range -40 to 40, high must be greater than low.\n\n");
+			printf("Incorrect values, temperatures must be in the range");
+			printf("-40 to 40, high must be greater than low.\n\n");
 
 			printf("Enter the high value for day %d: ", i+1);
 			scanf("%d", &high);
@@ -31,24 +32,25 @@ int main(void) //initializes the code
 			scanf("%d", &low);
 			printf("\n");
 			}
+ 
+		totHigh += high;                                                //storing the high temperatures
+		totLow += low;                                                  //storing the low temperatures
 
-		totHigh += high;  //storing the high temperatures
-		totLow += low;    //storing the low temperatures
-
-		if (high > highestT) {  //if the temp entered is higher than the previous one, it is stored in highestT (replaces it)
+		if (high > highestT) {                                          //if the temp entered is higher than the previous one,
+			                                                        //it is stored in highestT (replaces it)
 			highestT = high;    
-            highDay = i+1;   //shows the day in which the highest temp occured
+            highDay = i+1;                                                      //shows the day in which the highest temp occured
 		}
 
-		if (low < lowestT) {  //if the temp entered is lower than the previous one, it is stored in lowestT
+		if (low < lowestT) {                                            //if the temp entered is lower than the previous one, it is stored in lowestT
 			lowestT = low;
 	        lowDay = i+1;
 		}
 	} 
 
-	mean = (totHigh + totLow) / (NUMS * 2);  //calculates the mean temperature
-	meanHigh = totHigh / NUMS;  //calculates the mean for the high temperatures
-	meanLow = totLow / NUMS;    //calculates the mean for the low temperatures
+	mean = (totHigh + totLow) / (NUMS * 2);                                 //calculates the mean temperature
+	meanHigh = totHigh / NUMS;                                              //calculates the mean for the high temperatures
+	meanLow = totLow / NUMS;                                                //calculates the mean for the low temperatures
 	
 	printf("The average (mean) LOW temperature was: %.2f\n", meanLow);
 	printf("The average (mean) HIGH temperature was: %.2f\n", meanHigh);
@@ -56,5 +58,5 @@ int main(void) //initializes the code
 	printf("The highest temperature was %d, on day %d\n", highestT, highDay);
 	printf("The lowest temperature was %d, on day %d\n", lowestT, lowDay);
 
-	return 0; //ends code
+	return 0;                                                               //ends code
 }
