a09953af4ef4429bafd853d5303f1971791baa54
hnbreyer <53955491+hnbreyer@users.noreply.github.com>
2020-08-05 16:27:39 -0300

    Update Ship.h

--- OOP244/Week05_ShipEngine/Lab/Ship.h
+++ OOP244/Week05_ShipEngine/Lab/Ship.h
@@ -1,13 +1,4 @@
-#pragma once
-
 //Ship.h
-
-/**********************************************************
- * Name: Helen Nunes Breyer
- * Student ID: 120046198
- * Seneca email: hnunes-breyer@myseneca.ca
- * Section: NEE
- **********************************************************/
 
 #ifndef SDDS_SHIP_H
 #define SDDS_SHIP_H
@@ -23,18 +14,15 @@ namespace sdds
 {
     class Ship
     {
-
         Engine engineArr[MAX_ENGINE];
         char shipType[MAX_SHIP];
         int number_engines;
         float distance;
-       
-
     public:
         //constructors
         Ship();
         Ship(const char* shipT, Engine* engines, int lengthEngine);
-        //
+
         void setEmptyShip();
         void setShip(const char* type, Engine* engines, int lengthEngine);
         bool empty() const;
@@ -42,12 +30,9 @@ namespace sdds
         void display() const;
         Ship& operator+=(Engine e);
         friend bool operator==(const Ship& ship, const Ship& other_ship);
-       
-        
+         
     };
         bool operator<(const Ship& ship, double size);
 
 }
-
-
 #endif
