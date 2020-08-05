eab8250381a899ccb172c735268cdf2a80f4d748
hnbreyer <53955491+hnbreyer@users.noreply.github.com>
2020-08-05 16:49:27 -0300

    Update Engine.h

--- OOP244/Week05_ShipEngine/DIY/Engine.h
+++ OOP244/Week05_ShipEngine/DIY/Engine.h
@@ -1,18 +1,8 @@
-#pragma once
-
 //Engine.h
-
-/**********************************************************
- * Name: Helen Nunes Breyer
- * Student ID: 120046198
- * Seneca email: hnunes-breyer@myseneca.ca
- * Section: NEE
- **********************************************************/
 
 #ifndef SDDS_ENGINE_H
 #define SDDS_ENGINE_H
 
- //#include <iostream>
 #include <cstring>
 
 const int MAX_LENGTH = 30;
@@ -28,18 +18,13 @@ namespace sdds
         //constructors
         Engine();
         Engine(const char* t, double s);
+     
         void setEmptyEng();
         void setEng(const char* enT, double sz);
         double get() const;
         void display() const;
 
     };
-
-
-
-
-
 }
-
 
 #endif
