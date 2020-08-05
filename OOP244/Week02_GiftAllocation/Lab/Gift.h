7d687f01439de921c79b1dce411015c35ad76394
hnbreyer <53955491+hnbreyer@users.noreply.github.com>
2020-08-05 13:31:03 -0300

    Update Gift.h

--- OOP244/Week02_GiftAllocation/Lab/Gift.h
+++ OOP244/Week02_GiftAllocation/Lab/Gift.h
@@ -1,1 +1,30 @@
+//Gift.h
 
+#ifndef SDDS_GIFT_H
+#define SDDS_GIFT_H
+
+namespace sdds
+{
+	const int MAX_DESC = 15;
+	const double MAX_PRICE = 999.99;
+	using namespace std;
+
+	struct Gift
+	{
+		char g_description[MAX_DESC + 1];
+		double g_price;
+		int g_units;
+	};
+
+	void gifting(char *giftName);
+
+	void gifting(double &price);
+
+	void gifting(int &units);
+
+	void display(const Gift &gifts);
+
+}
+
+
+#endif
