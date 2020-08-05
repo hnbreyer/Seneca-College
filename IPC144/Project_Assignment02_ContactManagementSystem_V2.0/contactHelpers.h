6cd5e400510308d7a3b800d3081eb9ded2919694
hnbreyer <53955491+hnbreyer@users.noreply.github.com>
2020-08-05 12:18:29 -0300

    Update contactHelpers.h

--- IPC144/Project_Assignment02_ContactManagementSystem_V2.0/contactHelpers.h
+++ IPC144/Project_Assignment02_ContactManagementSystem_V2.0/contactHelpers.h
@@ -4,71 +4,47 @@ Description: This header file modularizes general helper functions to
 ----------------------------------------------------------------------*/
 
 
-// Hint:  This header file now has functions with parameters referring to 
-//        the struct Contact type so be sure to include the contacts.h header:
+//  This header file now has functions with parameters referring to 
+//  the struct Contact type so be sure to include the contacts.h header:
 #include "contacts.h"
 
 //--------------------------------
 // Function Prototypes
 //--------------------------------
 
-// +-------------------------------------------------+
-// | NOTE:  Copy/Paste your Assignment-2 Milestone-3 |
-// |        function prototypes here...              |
-// +-------------------------------------------------+
 
-// Clear the standard input buffer
-// clearKeyboard:
-void clearKeyboard(void);
+void clearKeyboard(void);                                                                // Clear the standard input buffer
 
-// pause:
 void pause(void);
 
-// getInt:
 int getInt(void);
 
-// getIntInRange:
 int getIntInRange(int min, int max);
 
-// yes:
 int yes(void);
 
-// menu:
 int menu(void);
 
-// contactManagerSystem:
 void contactManagerSystem(void);
 
-// Generic function to get a ten-digit phone number:
-// getTenDigitPhone:
-void getTenDigitPhone(char phoneNum[]);
+void getTenDigitPhone(char phoneNum[]);                                                // Generic function to get a ten-digit phone number
 
-// findContactIndex:
 int findContactIndex(const struct Contact contacts[], int size, const char cellNum[]);
 
-// displayContactHeader:
 void displayContactHeader(void);
 
-// displayContactFooter:
 void displayContactFooter(int count);
 
-// displayContact:
 void displayContact(const struct Contact* contact);
 
-// displayContacts:
 void displayContacts(const struct Contact contacts[], int size);
 
-// searchContacts:
 void searchContacts(const struct Contact contacts[], int size);
 
-// addContact:
 void addContact(struct Contact contacts[], int size);
 
-// updateContact:
 void updateContact(struct Contact contacts[], int size);
 
-// deleteContact:
 void deleteContact(struct Contact contacts[], int size);
 
-// sortContacts:
 void sortContacts(struct Contact contacts[], int size);
