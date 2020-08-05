53c84e5164dc86420bef378ace673d532f486f59
hnbreyer <53955491+hnbreyer@users.noreply.github.com>
2020-08-05 13:37:31 -0300

    Update GiftMain.cpp

--- OOP244/Week02_GiftAllocation/Lab/GiftMain.cpp
+++ OOP244/Week02_GiftAllocation/Lab/GiftMain.cpp
@@ -62,3 +62,50 @@ int main() {
 
   return 0;
 }
+
+/* OUTPUT
+Enter number of Gifts to allocate: 4
+Gift #1:
+Enter gift description: Roses
+Enter gift price: 5.99
+Enter gift units: 5
+Gift #2:
+Enter gift description: Gift-card
+Enter gift price: -99.99
+Gift price must be between 0 and 999.99
+Enter gift price: 1000
+Gift price must be between 0 and 999.99
+Enter gift price: 50Enter gift units: 1
+Gift #3:
+Enter gift description: Hugs
+Enter gift price: 0
+Enter gift units: -5
+Gift units must be at least 1
+Enter gift units: 0
+Gift units must be at least 1
+Enter gift units: 25
+Gift #4:
+Enter gift description: cardcardcardcard
+Enter gift price: 2
+Enter gift units: 3
+Gift #1:
+Gift Details:
+Description: Roses
+Price: 5.99
+Units: 5
+Gift #2:
+Gift Details:
+Description: Gift-card
+Price: 50
+Units: 1
+Gift #3:
+Gift Details:
+Description: Hugs
+Price: 0
+Units: 25
+Gift #4:
+Gift Details:
+Description: cardcardcardcar
+Price: 2
+Units: 3
+*/
