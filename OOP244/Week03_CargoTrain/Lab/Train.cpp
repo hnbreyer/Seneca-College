f1fa807b7ac24c9893d1739557e8b712e9d61625
hnbreyer <53955491+hnbreyer@users.noreply.github.com>
2020-08-05 13:55:10 -0300

    Create Train.cpp

---
+++ OOP244/Week03_CargoTrain/Lab/Train.cpp
@@ -0,0 +1,87 @@
+//Train.cpp
+
+/**********************************************************
+ * Name: Helen Nunes Breyer
+ * Student ID: 120046198
+ * Seneca email: hnunes-breyer@myseneca.ca
+ * Section: NEE
+ **********************************************************/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <iostream>
+#include "Train.h"
+using namespace std;
+
+namespace sdds
+{
+    void Train::setTrain(const char* nam, int i_d) {
+        if (nam != nullptr && nam[0] != '\0' && i_d >= 1) {
+
+            id = i_d;
+            strcpy(name,nam);
+        }
+        else {
+
+            id = 0;
+            name[0] = '\0';  
+        }
+
+        cargo = nullptr;
+
+    }
+
+    bool Train::isEmpty() const {
+        bool empty;
+        if (name[0] == '\0' && id == 0) { 
+
+            empty = true;
+        }
+        else {
+            empty = false;
+        }
+
+        return empty;
+    }
+
+    void Train::display() const {
+
+        cout << "***Train Summary***" << endl;
+
+            if (isEmpty()) {
+                cout << "This is an empty train." << endl;
+                
+            }
+            else {
+                cout << "Name: " << name << " ID: " << id << endl;
+                
+
+                if (cargo == nullptr) {
+                    cout << "No cargo on this train." << endl;
+                }
+                else {
+                    cout << "Cargo: " << cargo->description << " Weight: " << cargo->weight << endl;
+                }
+            }  
+    }
+
+    void Train::loadCargo(Cargo carg) {
+
+        //Cargo* carg = nullptr;
+
+        cargo = new Cargo;
+
+        strcpy(cargo->description, carg.description);
+        cargo->weight = carg.weight;
+
+        
+    }
+
+    void Train::unloadCargo() {
+
+        
+        delete cargo;
+        cargo = nullptr;
+    }
+}
