ad4880cee47faf3bfe0c2de16a615ac0629ec355
hnbreyer <53955491+hnbreyer@users.noreply.github.com>
2020-08-05 12:14:04 -0300

    Update contacts.h

--- IPC144/Project_Assignment02_ContactManagementSystem_V2.0/contacts.h
+++ IPC144/Project_Assignment02_ContactManagementSystem_V2.0/contacts.h
@@ -1,51 +1,34 @@
-// ----------------------------------------------------------------------------
-// DO NOT remove the below 2 lines.
-// The below two lines is needed to ensure only one copy
-// of this header file is included when compiling the project.
-// (it is referred to as "safeguarding")
-// You will learn about this in C++ (OOP244/BTP200)
-// For now, simply leave this code in!
-#ifndef CONTACTS_H_
-#define CONTACTS_H_
-// ----------------------------------------------------------------------------
+#ifndef CONTACTS_H_                                                           //safeguards - ensure only one copy of header file
+#define CONTACTS_H_                                                           //is included when compiling
 
 //--------------------------------
 // Structure Types
 //--------------------------------
 
-// +-------------------------------------------------+
-// | NOTE:  Copy/Paste your Assignment-2 Milestone-3 |
-// |        structures below:                        |
-// +-------------------------------------------------+
-
-// Structure type Name declaration
-struct Name
+struct Name                                                                  // Structure type Name declaration
 {
 	char firstName[31];
 	char middleInitial[7];
 	char lastName[36];
 };
 
-// Structure type Address declaration
-struct Address
+struct Address                                                              // Structure type Address declaration
 {
-	int streetNumber; //changed from unsigned to int to show error message as per assigment output
+	int streetNumber;                                                   //changed from unsigned to int to show error message as per assigment output
 	char street[41];
 	int apartmentNumber;
 	char postalCode[8];
 	char city[41];
 };
 
-// Structure type Numbers declaration
-struct Numbers
+struct Numbers                                                              // Structure type Numbers declaration
 {
 	char cell[11];
 	char home[11];
 	char business[11];
 };
 
-// Structure type Contact declaration
-struct Contact
+struct Contact                                                              // Structure type Contact declaration
 {
 	struct Name name;
 	struct Address address;
@@ -57,24 +40,15 @@ struct Contact
 // Function Prototypes
 //--------------------------------
 
-// +-------------------------------------------------+
-// | NOTE:  Copy/Paste your Assignment-2 Milestone-3 |
-// |        function prototypes below...             |
-// +-------------------------------------------------+
 
-// getName:
 void getName(struct Name*);
 
-// getAddress:
 void getAddress(struct Address*);
 
-// getNumbers:
 void getNumbers(struct Numbers*);
 
-// getContact:
 void getContact(struct Contact*);
 
 
 // ----------------------------------------------------------------------------
-// DO NOT remove the below line (closing of the "safeguard" condition)
-#endif // !CONTACTS_H_
+#endif // !CONTACTS_H_                                                      //closing of the safeguard condition
