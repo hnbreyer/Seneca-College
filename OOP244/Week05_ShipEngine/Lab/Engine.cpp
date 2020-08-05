da4c0d4a12a8656209a87a6457cef70ba230355b
hnbreyer <53955491+hnbreyer@users.noreply.github.com>
2020-08-05 16:26:13 -0300

    Create Engine.cpp

---
+++ OOP244/Week05_ShipEngine/Lab/Engine.cpp
@@ -0,0 +1,73 @@
+//Engine.cpp
+
+/**********************************************************
+ * Name: Helen Nunes Breyer
+ * Student ID: 120046198
+ * Seneca email: hnunes-breyer@myseneca.ca
+ * Section: NEE
+ **********************************************************/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <iostream>
+#include "Engine.h"
+#include "Ship.h"
+
+using namespace std;
+
+namespace sdds
+{
+    Engine::Engine() {
+        setEmptyEng();
+    }
+
+   
+
+    Engine::Engine(const char* t, double s) {
+    
+        
+         if (strlen(t) < 1 && s < 0) {
+        setEmptyEng();
+         }
+         else {
+             setEng(t, s);
+         }
+        
+    }
+
+
+    void Engine::setEmptyEng() {
+
+        engineType[0] = '\0';
+        size = 0;
+    }
+
+    void Engine::setEng(const char* enT, double sz){
+        if (enT != nullptr && enT[0] != '\0' && sz > 0) {
+
+            strcpy(engineType, enT);
+            size = sz;
+        }
+        else {
+            setEmptyEng();
+        }
+    }
+
+    double Engine::get() const {
+        return size;
+    }
+
+
+    void Engine::display() const {
+
+        if (engineType != nullptr && engineType[0] != '\0' && size > 0) {
+
+            cout << size << " liters - " << engineType << endl;
+
+        }
+
+    }
+
+   
+}
