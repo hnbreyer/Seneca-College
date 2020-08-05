c95d5596fb83b88fe18cbfeee3504a524272dfc2
hnbreyer <53955491+hnbreyer@users.noreply.github.com>
2020-08-05 16:24:53 -0300

    Create Ship.h

---
+++ OOP244/Week05_ShipEngine/Lab/Ship.h
@@ -0,0 +1,53 @@
+#pragma once
+
+//Ship.h
+
+/**********************************************************
+ * Name: Helen Nunes Breyer
+ * Student ID: 120046198
+ * Seneca email: hnunes-breyer@myseneca.ca
+ * Section: NEE
+ **********************************************************/
+
+#ifndef SDDS_SHIP_H
+#define SDDS_SHIP_H
+
+#include <cstring>
+
+#include "Engine.h"
+
+const int MAX_ENGINE = 10;
+const int MAX_SHIP = 7;
+
+namespace sdds
+{
+    class Ship
+    {
+
+        Engine engineArr[MAX_ENGINE];
+        char shipType[MAX_SHIP];
+        int number_engines;
+        float distance;
+       
+
+    public:
+        //constructors
+        Ship();
+        Ship(const char* shipT, Engine* engines, int lengthEngine);
+        //
+        void setEmptyShip();
+        void setShip(const char* type, Engine* engines, int lengthEngine);
+        bool empty() const;
+        float calculatePower() const;
+        void display() const;
+        Ship& operator+=(Engine e);
+        friend bool operator==(const Ship& ship, const Ship& other_ship);
+       
+        
+    };
+        bool operator<(const Ship& ship, double size);
+
+}
+
+
+#endif
