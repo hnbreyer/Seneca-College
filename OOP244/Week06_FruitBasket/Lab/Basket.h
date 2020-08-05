862dde86c23df7d6d3c885a64389cf54079423f7
hnbreyer <53955491+hnbreyer@users.noreply.github.com>
2020-08-05 17:12:15 -0300

    Update Basket.h

--- OOP244/Week06_FruitBasket/Lab/Basket.h
+++ OOP244/Week06_FruitBasket/Lab/Basket.h
@@ -14,10 +14,9 @@ namespace sdds
         int m_qty;
         double m_price;
     public:
-        Basket(); //default constructor
-        Basket(const char* fruitNam, int qty, double prix); // 3 arg constructor
-      
-        ~Basket(); //destructor
+        Basket();                                                                                     //default constructor
+        Basket(const char* fruitNam, int qty, double prix);                                           // 3 arg constructor
+        ~Basket();                                                                                    //destructor
         void setName(const char* fName);
         void setQty(int qt);
         void setPrice(double prc);
@@ -26,13 +25,10 @@ namespace sdds
 
         std::ostream& display(std::ostream& os) const;
 
-        Basket(const Basket& bas); // copy constructor
-        Basket& operator=(const Basket& basket); //copy assignment operator
+        Basket(const Basket& bas);                                                                  // copy constructor
+        Basket& operator=(const Basket& basket);                                                    //copy assignment operator
         bool operator==(const Basket& other_bas);
         bool operator!=(const Basket& other_b);
-
-       
-
     };
         std::ostream& operator<<(std::ostream& os, const Basket& bsk);
 }
