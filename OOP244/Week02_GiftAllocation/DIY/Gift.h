c88d4d6072803f111e8e8e95de1ca6b119c42f27
hnbreyer <53955491+hnbreyer@users.noreply.github.com>
2020-08-05 13:33:39 -0300

    Create Gift.h

---
+++ OOP244/Week02_GiftAllocation/DIY/Gift.h
@@ -0,0 +1,55 @@
+// Gift.h
+
+/**********************************************************
+ * Name: Helen Nunes Breyer
+ * Student ID: 120046198
+ * Seneca email: hnunes-breyer@myseneca.ca
+ * Section: NEE
+ **********************************************************/
+
+#ifndef SDDS_GIFT_H
+#define SDDS_GIFT_H
+
+namespace sdds
+{
+	const int MAX_DESC = 15;
+	const int MAX_PAT = 20;
+	const double MAX_PRICE = 999.99;
+
+	using namespace std; //included for cin and cout functions in Gift.cpp
+
+	struct Gift
+	{
+		char g_description[MAX_DESC + 1] = { 0 };
+		double g_price;
+		int g_units;
+		int wrap_layers;
+		struct Wrapping* wrap;
+
+	};
+
+	struct Wrapping
+	{
+		char* pattern;
+	};
+
+	void gifting(char* giftName);
+
+	void gifting(double& price);
+
+	void gifting(int& units);
+
+	void gifting(Gift& gift); //new gifting function
+
+	void display(const Gift& gifts);
+
+	bool wrap(Gift& gifts);
+
+	bool unwrap(Gift& gifts);
+	
+
+
+}
+
+
+#endif
