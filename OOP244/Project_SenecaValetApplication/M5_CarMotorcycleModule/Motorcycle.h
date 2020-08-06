b352900c7571b3b40b03022fd44ae6864e8e20d7
hnbreyer <53955491+hnbreyer@users.noreply.github.com>
2020-08-06 15:28:57 -0500

    Create Motorcycle.h

---
+++ OOP244/Project_SenecaValetApplication/M5_CarMotorcycleModule/Motorcycle.h
@@ -0,0 +1,34 @@
+/**********************************************************
+ * Name: Helen Nunes Breyer
+ * Student ID: 120046198
+ * Seneca email: hnunes-breyer@myseneca.ca
+ * Section: NEE
+ **********************************************************/
+ //Motorcycle.h
+
+#ifndef SDDS_MOTORCYCLE_H
+#define SDDS_MOTORCYCLE_H
+
+
+#include <iostream>
+#include <cstring>
+
+#include "Vehicle.h"
+
+namespace sdds
+{
+    class Motorcycle : public Vehicle
+    {
+        bool sidecar;
+    public:
+        Motorcycle();
+        Motorcycle(const char*, const char*);
+        void setEmpty();
+        Motorcycle(Motorcycle&) = delete; //copy
+        Motorcycle& operator=(Motorcycle&) = delete; //copy assignment
+        std::istream& read(std::istream& istr = std::cin);
+        std::ostream& write(std::ostream& ostr = std::cout) const;
+    };
+}
+
+#endif
