e5022910c58badf237ac9536c45e3da7e6b98eaa
hnbreyer <53955491+hnbreyer@users.noreply.github.com>
2020-08-06 13:41:03 -0500

    Create Doctor.h

---
+++ OOP244/Week08_EmployeeSystem/Doctor.h
@@ -0,0 +1,41 @@
+//Doctor.h
+
+/**********************************************************
+ * Name: Helen Nunes Breyer
+ * Student ID: 120046198
+ * Seneca email: hnunes-breyer@myseneca.ca
+ * Section: NEE
+ **********************************************************/
+#ifndef SDDS_DOCTOR_H__
+#define SDDS_DOCTOR_H__
+
+#include <cstring>
+#include "Employee.h"
+
+const int MIN_W_HOURS = 6;
+const int MAX_CHAR = 20;
+
+namespace sdds
+{
+    class Doctor :public Employee
+    {
+        char m_type[MAX_CHAR + 1];
+        double m_salary;
+        int m_whours;
+        bool m_specialist;
+    public:
+        Doctor();
+        Doctor(const char* type, double sal, int hours, bool spec = false);
+        void setSalary(double sal);
+        bool workHours();
+        void bonus();
+        std::ostream& display(std::ostream& os) const;
+        //added functions
+        void setEmpty();
+        //virtual ~Doctor();
+    };
+
+}
+
+
+#endif 
