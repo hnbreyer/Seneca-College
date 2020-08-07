b3db0dd5994694b5addb5e382c62a79a3218f44e
hnbreyer <53955491+hnbreyer@users.noreply.github.com>
2020-08-07 11:22:20 -0500

    Update Menu.h

--- OOP244/Project_SenecaValetApplication/M6_completedApp/Menu.h
+++ OOP244/Project_SenecaValetApplication/M6_completedApp/Menu.h
@@ -1,12 +1,4 @@
-/**********************************************************
- * Name: Helen Nunes Breyer
- * Student ID: 120046198
- * Seneca email: hnunes-breyer@myseneca.ca
- * Section: NEE
-  ///I have done all the coding by myself and only copied the code that my professor provided to complete this program///
- **********************************************************/
-
- //Menu.h
+//Menu.h
 
 #ifndef SDDS_MENU_H
 #define SDDS_MENU_H
@@ -17,22 +9,17 @@
 const int MAX_NO_OF_ITEMS = 10;
 namespace sdds
 {
-    class Menu; //forward declaration
+    class Menu;                                                                                    //forward declaration
     class MenuItem
     {
         char* m_itemName;
         MenuItem();
         MenuItem(const char* nam);
-        //member functions
         std::ostream& display(std::ostream& os = std::cout) const;
-        //copy and copy assignment prohibited
-        MenuItem(const MenuItem&) = delete;
+        MenuItem(const MenuItem&) = delete;                                                       //copy and copy assignment prohibited
         MenuItem& operator=(const MenuItem&) = delete;
-        //destruction
-        ~MenuItem();
-        //friend class
-        friend class Menu;
-
+        ~MenuItem();                                                                              //destruction
+        friend class Menu;                                                                        //friend class
     };
 
     class Menu
@@ -40,9 +27,7 @@ namespace sdds
         char* m_title;
         MenuItem* m_menuArr[MAX_NO_OF_ITEMS] = { nullptr };
         int m_indent;
-        //added member
-        int number_menuItems;
-
+        int number_menuItems;                                                                    //added member
     public:
         Menu(const char* t, int indent = 0);
         void init(const Menu& menu);
@@ -57,8 +42,7 @@ namespace sdds
         int run() const;
         operator int() const;
         ~Menu();
-        //added functions
-        void setEmpty();
+        void setEmpty();                                                                        //added functions
     };
 }
 
